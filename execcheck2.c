0dae711143cc5139f360dde871dfbb128705013c
vetrivel20 <trickyboi2007@gmail.com>
2026-03-09 09:24:03 +0530

    COMPARING FILES

--- execcheck2.c
+++ execcheck2.c
@@ -1,1 +1,25 @@
 //C Program to execute Linux system commands using Linux API system calls exec() family
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        printf("Child process executing ls command\n");
+        execlp("ls", "ls", NULL);
+        perror("Exec failed");
+        exit(1);
+    }
+    else
+    {
+        wait(NULL);
+        printf("Parent process: Child execution completed\n");
+    }
+
+    return 0;
+}
--- forkcheck.c
+++ forkcheck.c
@@ -1,1 +1,30 @@
 //C Program to create new process using Linux API system calls fork() and exit()
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork failed\n");
+    }
+    else if (pid == 0)
+    {
+        // Child process
+        printf("Child Process\n");
+        printf("Child PID  : %d\n", getpid());
+        printf("Parent PID : %d\n", getppid());
+    }
+    else
+    {
+        // Parent process
+        printf("Parent Process\n");
+        printf("Parent PID : %d\n", getpid());
+    }
+
+    return 0;
+}
